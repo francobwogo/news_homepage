3ac638da73ee23cfeeeb25e5d258425869f299fc
Francis Obwogo <francobwogo@gmail.com>
2023-12-23 13:21:24 +0200

    css update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function myFunction() {
     let navMenu = document.getElementById("navMenu");
     let menuIcon = document.getElementById("menuIcon");
     let closeIcon = document.getElementById("closeIcon");
+    let nav = document.getElementById("nav");
 
     if (menu.style.display === "flex") {
         menuIcon.style.display = "block";
@@ -10,12 +11,13 @@ function myFunction() {
         menu.style.display = "none";
         navMenu.style.position = "static";
         navMenu.style.backgroundColor = "none";
-
+        nav.style.paddingTop = "10px"
     } else {
         menuIcon.style.display = "none";
         closeIcon.style.display = "block";
         menu.style.display = "flex";
         navMenu.style.position = "fixed";
         navMenu.style.backgroundColor = "var(--clr-neutral-100)";
+        nav.style.paddingTop = "0"
     }
 }
